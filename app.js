9ef58f77bf25b870961414fa75ae6dfa5802d984
Alex Simisi Nta <smsxander@gmail.com>
2024-01-13 02:07:26 +0100

    update app.js

--- app.js
+++ app.js
@@ -45,11 +45,6 @@ app.get("/api", (req, res) => {
   });
 });
 
-// Gestion des erreurs 404
-app.use((req, res, next) => {
-  res.status(404).json({ error: 'Not Found' });
-});
-
 // Gestion des erreurs
 app.use((err, req, res, next) => {
   console.error(err.stack);
