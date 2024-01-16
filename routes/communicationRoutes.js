f86082a43f134e670c731661a21280a55bbbacbe
Alex Simisi Nta <smsxander@gmail.com>
2024-01-16 21:57:12 +0100

    update communicationRoutes

--- routes/communicationRoutes.js
+++ routes/communicationRoutes.js
@@ -5,5 +5,13 @@ const communicationController = require('../controllers/communicationController'
 // Routes pour la communication en temps réel
 router.get('/communications/:caseId', communicationController.getCommunicationsByCaseId);
 router.post('/communications', communicationController.addCommunication);
+router.post('/send-message', (req, res) => {
+    const { message } = req.body;
+
+    // Envoyer le message à tous les clients connectés via WebSocket
+    req.io.emit('new-message', { message });
+
+    res.status(200).json({ success: true })
+})
 
 module.exports = router;
