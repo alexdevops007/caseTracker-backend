7a690ca303937701925a4d3f4f95883ef75a4b2b
Alex Simisi Nta <smsxander@gmail.com>
2024-01-16 22:44:44 +0100

    update report

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,4 +1,5 @@
-const ReportModel = require('../models/ReportModel');
+const ReportModel = require("../models/ReportModel");
+const PDFDocument = require("pdfkit");
 
 // Récupérer tous les rapports d'un cas
 exports.getReportsByCaseId = async (req, res, next) => {
@@ -9,20 +10,29 @@ exports.getReportsByCaseId = async (req, res, next) => {
     res.status(200).json(reports);
   } catch (error) {
     console.error(`Error getting reports for case: ${error.message}`);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-// Générer un nouveau rapport
-exports.generateReport = async (req, res, next) => {
-  const { content, caseId, generatedBy } = req.body;
-
+// Fonction pour générer un rapport PDF
+exports.generateReport = async (req, res) => {
   try {
+    const doc = new PDFDocument();
+    // Logique pour la génération du rapport
     const newReport = new ReportModel({ content, caseId, generatedBy });
     const savedReport = await newReport.save();
     res.status(201).json(savedReport);
+
+    // PDF généré
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "attachment; filename=report.pdf");
+    doc.pipe(res);
+
+    // Ajout le contenu du rapport ici
+    doc.text("Rapport généré avec succès.");
+    doc.end();
   } catch (error) {
-    console.error(`Error generating report: ${error.message}`);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Erreur lors de la génération du rapport: ", error);
+    res.status(500).json({ success: false, error: error.message });
   }
 };
