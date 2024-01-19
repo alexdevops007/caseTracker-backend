8876f028f4ccce261c02a406d494f4009bc054df
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2024-01-19 02:02:36 +0100

    Update evidenceRoutes.js

--- routes/evidenceRoutes.js
+++ routes/evidenceRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const evidenceController = require("../controllers/evidenceController");
 
 // Routes pour la gestion des preuves numériques
+router.get('/evidences', evidenceController.getAllEvidences);
 router.get("/evidence/:caseId", evidenceController.getEvidenceByCaseId);
 router.post("/evidence", evidenceController.addEvidence);
 router.post("/evidence/upload", evidenceController.uploadEvidence);
